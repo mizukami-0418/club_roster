1e3214a49bb6545bf1333cc3febc3e58dcb08433
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-07-29 22:17:07 +0900

    DEBUGの設定を変更

--- club_roster/settings.py
+++ club_roster/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
